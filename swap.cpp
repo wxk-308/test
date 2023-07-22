3b36dd04e880d4fe5f73dbccb5f9d3d5c539ae73
wxk-308 <2442564348@qq.com>
2023-07-22 11:39:35 +0800

    third

---
+++ swap.cpp
@@ -0,0 +1,6 @@
+#include "swap.h"
+void swap(int &a,int &b){
+    int tmp=a;
+    a=b;
+    b=tmp;
+}
--- world.cpp
+++ world.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
-
+// #include "swap.h"
 using namespace std;
 
+void swap(int &a,int &b){
+    int tmp=a;
+    a=b;
+    b=tmp;
+}
 int main(){
-    cout<<'hello'<<endl;
-    return ;
+    int a=1,b=10;
+    cout<<a<<' '<<b<<endl;
+    swap(a,b);
+    cout<<a<<' '<<b<<endl;
+    return 0;
 }
